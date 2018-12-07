2c05786c8d6b75c65265fe86ea4638bcefafef87
DagmaraNowak <DagmaraNowak@users.noreply.github.com>
2018-12-07 20:23:27 +0100

    Format

---
+++ ex2.py
@@ -0,0 +1,8 @@
+print("Hello World")
+name = 'Dagmara'
+print("Hello World",name)
+message = 'Hello {0:s} and {1:s}!'
+message.format('Dagmara','Kuba')
+print(message)
+message1 = "Kuba ma {0:d} pieskow"
+print(message1.format(6))
