a2811ee39344101a58bc5d4d109ddcf6eabe2f80
DagmaraNowak <DagmaraNowak@users.noreply.github.com>
2018-12-07 20:45:25 +0100

    Dodanie pozegnania

--- ex2.py
+++ ex2.py
@@ -6,3 +6,6 @@ message.format('Dagmara','Kuba')
 print(message)
 message1 = "Kuba ma {0:d} pieskow"
 print(message1.format(6))
+message2 = "Goodbye %s and %s"
+print(message2 % ('Dagmara','Kuba'))
+print((message2 % ('Dagmara','Kuba')).upper())
